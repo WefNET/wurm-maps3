08f52d565e89d66ac2e3fe5db414cdf996bf26a7
WefNET <john.wefler@gmail.com>
2019-01-01 13:26:37 -0600

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { XanaduComponent } from './maps/xanadu/xanadu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'xanadu',
+    component: XanaduComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService } from 'primeng/api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XanaduComponent } from './maps/xanadu/xanadu.component';
+
+import { CommonService } from './services/common.service'
+import { StyleService } from './services/style.service'
+import { SheetsService } from './services/sheets.service'
+import { LayersService } from './services/layers.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XanaduComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ButtonModule,
+    SelectButtonModule,
+    SidebarModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    CommonService,
+    StyleService,
+    SheetsService,
+    LayersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
